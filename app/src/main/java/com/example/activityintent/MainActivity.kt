d647c6473ee93498b49b13a8e32197243a08e749
Gabriel Reifegerste <gabriel.reifegerste@southsystem.com.br>
2021-10-25 01:16:57 -0300

    first commit

---
+++ app/src/main/java/com/example/activityintent/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.activityintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnOpenActivity.setOnClickListener {
+            Intent(this, SecondActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+}
